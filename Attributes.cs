27d7627317d93e78ead0450f2a5ca71c46c490f2
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2023-06-26 19:23:50 +0500

    Add project files.

---
+++ Attributes.cs
@@ -0,0 +1,96 @@
+﻿using Reflection;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class FileConfigurationItemAttribute : Attribute
+    {
+        public readonly string _settingName;
+
+        public FileConfigurationItemAttribute(string settingName)
+        {
+            _settingName = settingName;
+        }
+
+        public string Setting
+        {
+            get { return GetSettingValue(); }
+            set { SetSettingValue(_settingName, value); }
+        }
+
+        public string GetSettingValue()
+        {
+            string configFilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "config.txt");
+            string[] lines = File.ReadAllLines(configFilePath);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split('=');
+                if (parts.Length == 2 && parts[0].Trim() == _settingName)
+                {
+                    return parts[1].Trim();
+                }
+            }
+            return "Not Found";
+        }
+
+        public void SetSettingValue(string key, string value)
+        {
+            string configFilePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, "config.txt");
+            string[] lines = File.ReadAllLines(configFilePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split('=');
+                if (parts.Length == 2 && parts[0].Trim() == key)
+                {
+                    lines[i] = key + " = " + value;
+                    File.WriteAllLines(configFilePath, lines);
+                    return;
+                }
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class ConfigurationManagerConfigurationItemAttribute : Attribute
+    {
+        public readonly string _settingName;
+
+        public ConfigurationManagerConfigurationItemAttribute(string settingName)
+        {
+            _settingName = settingName;
+        }
+
+        public string Setting
+        {
+            get { return GetSettingValue(); }
+            set { SetSettingValue(_settingName, value); }
+        }
+
+        private string GetSettingValue()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            return appSettings[_settingName] ?? "Not found";
+        }
+
+        private static void SetSettingValue(string key, string value)
+        {
+            var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            var settings = configFile.AppSettings.Settings;
+            if (settings[key] == null)
+            {
+                settings.Add(key, value);
+            }
+            else
+            {
+                settings[key].Value = value;
+            }
+            configFile.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MemberInfo info = typeof(Setting);
+            var attributes = info.GetCustomAttributes(true);
+
+            foreach (var attribute in attributes)
+            {
+                if (attribute is ConfigurationManagerConfigurationItemAttribute)
+                {
+                    var setting = attribute as ConfigurationManagerConfigurationItemAttribute;
+                    if (setting != null)
+                    {
+                        Console.WriteLine($"Setting '{setting._settingName}': {setting.Setting}");
+                        setting.Setting = $"new value blah.blah";
+                        Console.WriteLine($"Updated value for setting '{setting._settingName}': {setting.Setting}");
+                    }
+                }
+                if (attribute is FileConfigurationItemAttribute)
+                {
+                    var setting = attribute as FileConfigurationItemAttribute;
+                    if (setting != null)
+                    {
+                        Console.WriteLine($"Setting '{setting._settingName}': {setting.Setting}");
+                        setting.Setting = $"new value 7";
+                        Console.WriteLine($"Updated value for setting '{setting._settingName}': {setting.Setting}");
+                    }
+                }
+            }
+
+        }
+    }
+
+
+    [ConfigurationManagerConfigurationItemAttribute("BatchFile")]
+    [FileConfigurationItemAttribute("LogLevel")]
+    public class Setting
+    {
+    }
+}
